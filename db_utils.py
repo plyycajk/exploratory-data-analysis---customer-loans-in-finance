c61347be3f67d5dd038a402d6dad3eba177aeab1
plyycajk <colinknight1985@gmail.com>
2023-10-31 11:20:30 +0000

    adds load_credentials function to db_utils.py

--- db_utils.py
+++ db_utils.py
@@ -1,4 +1,12 @@
+import os
 import yaml
 
 
-class  RDSDatabaseConnector:
+#loads yaml based on filename without extension
+def load_credentials(filename):
+    with open (f'{filename}.yaml','r') as f:
+        loaded_credentials = yaml.safe_load(f)
+        print(loaded_credentials)
+
+load_credentials('credentials')
+#class  RDSDatabaseConnector:
