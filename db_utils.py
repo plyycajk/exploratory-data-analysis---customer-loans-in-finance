d41233c50d5ab219088fd25c734ba6c0136b678f
plyycajk <colinknight1985@gmail.com>
2023-11-01 13:34:37 +0000

    completes RDSDatabaseConnector class

--- db_utils.py
+++ db_utils.py
@@ -1,12 +1,58 @@
-import os
+#imports required for script
 import yaml
+from sqlalchemy import create_engine, text
+import pandas as pd
+from sqlalchemy import inspect
 
 
 #loads yaml based on filename without extension
 def load_credentials(filename):
     with open (f'{filename}.yaml','r') as f:
         loaded_credentials = yaml.safe_load(f)
-        print(loaded_credentials)
+        return loaded_credentials
 
-load_credentials('credentials')
-#class  RDSDatabaseConnector:
+class  RDSDatabaseConnector(object):
+    
+    """
+    This class docstring needs to be completed 
+    
+    Attributes:
+        attributes need to be described in detail here
+    """
+
+    def __init__(self, connection_credentials):
+        self.db_credentials = load_credentials(connection_credentials)
+        self.user = self.db_credentials['RDS_USER']
+        self.password = self.db_credentials['RDS_PASSWORD']
+        self.host = self.db_credentials['RDS_HOST']
+        self.port = self.db_credentials['RDS_PORT']
+        self.database = self.db_credentials['RDS_DATABASE']
+        self.engine = None
+        self.dbconn = None
+
+        print('connection credentials loaded')
+    
+    def __enter__(self):
+        self.engine = create_engine(f"postgresql+psycopg2://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}")
+        print('creating engine')
+        self.dbconn = self.engine.connect()
+        print('establishing connection')
+        return self.dbconn
+
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('closing connection')
+        self.dbconn.close()
+        print('data extracted')
+
+
+def loan_payments_to_dataframe(dbcredentials):
+    with RDSDatabaseConnector(dbcredentials) as conn:
+        df = pd.read_sql_table('loan_payments', conn)
+        print('loan_payments data extracted and available in \'loan_payments\' dataframe') 
+        return df
+
+
+if __name__ == '__main__':
+    loan_payments = loan_payments_to_dataframe('credentials')
+
