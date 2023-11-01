97578731ac09d0cae2fa458acef7d3feec207bb6
plyycajk <colinknight1985@gmail.com>
2023-11-01 22:42:20 +0000

    updates README.md with project information and adds docstrings to db_utils.py

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # exploratory-data-analysis---customer-loans-in-finance
+
+## Contents
+1. [Description](#description)
+1. [Installation instructions](#installation-instructions)
+1. [Usage instructions](#usage-instructions)
+1. [File structure](#file-structure)
+1. [License information](#license-information)
+
+## Description
+This project accesses a relational database and extracts data for exploratory data analysis.
+
+I learnt how to use SQLAlchemy to connect to a database as well as how to structure this within a class by use of the  __ enter __ and __ exit __ magic methods.
+
+I also learnt how to keep login details out of scripts by use of the separate .yaml file in the directory, and using .gitignore to prevent this information being shared to the Github repository.
+
+## Installation instructions
+
+This project requires the following packages to be installed in order to run correctly:
+
+- pandas
+- SQLAlchemy
+- Pyyaml
+
+In order to establish a connection to the database, db_utils. py requires a .yaml file (with database connection credentials) to be saved in the same directory. Values need to be populated against the following keys:
+
+- RDS_HOST: {link/id for the database}
+- RDS_PASSWORD: {password for the user to access the  database}
+- RDS_USER: {username for the user to access the  database} 
+- RDS_DATABASE: {name of database}
+- RDS_PORT: {port to be accessed}
+
+## Usage instructions
+
+A yaml file with database log-in credentials is required in order to establish the database. This should be stored in the same directory as the scripts.
+- running db_utils.py as a script will require this file to be called 'credentials.yaml'. The class is written in such a way to allow any named yaml file to be called when the class is imported as a module
+
+## File structure
+
+### db_utils.py
+
+This file initiates a class (RDSDatabaseConnector) which establishes a connection with the database. 
+Subsequent functions in this file run a specific query in the database to extract loan patyment information and save this to a .csv file (loan_payments.csv).
+
+
+## License information
+None
--- db_utils.py
+++ db_utils.py
@@ -1,39 +1,56 @@
 #imports required for script
-import yaml
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
 import pandas as pd
-from sqlalchemy import inspect
+import yaml
 
 
-#loads yaml based on filename without extension
 def load_credentials(filename):
+    '''This function loads a .yaml file containing database credential information'''
     with open (f'{filename}.yaml','r') as f:
         loaded_credentials = yaml.safe_load(f)
         return loaded_credentials
 
 class  RDSDatabaseConnector(object):
-    
-    """
-    This class docstring needs to be completed 
+    '''
+    This class establishes a connection to relational database
     
     Attributes:
-        attributes need to be described in detail here
-    """
+        self.db_credentials (dict): database credentials to allow connection, loaded from load_credentials() function
+        self.user (str): username for user connecting (loaded from credentials file)
+        self.password (str): password for user connecting (loaded from credentials file)
+        self.host (str): host info for the database being connected to (loaded from credentials file)
+        self.database (str): database name of database being connected to (loaded from credentials file)
+        self.db_type (str): type of database being connected to
+        self.dbapi (str): chosen database API for connection
+        self.engine (obj): SQLAlchemy engine to establish connection to the database
+        self.dbconn (obj): database connection object
+    '''
 
     def __init__(self, connection_credentials):
+        '''
+        See help(RDSDatabaseConnector) for accurate signature
+        '''
         self.db_credentials = load_credentials(connection_credentials)
         self.user = self.db_credentials['RDS_USER']
         self.password = self.db_credentials['RDS_PASSWORD']
         self.host = self.db_credentials['RDS_HOST']
         self.port = self.db_credentials['RDS_PORT']
         self.database = self.db_credentials['RDS_DATABASE']
+        self.db_type = 'postgresql'
+        self.dbapi =  'psycopg2'
         self.engine = None
         self.dbconn = None
 
         print('connection credentials loaded')
     
     def __enter__(self):
-        self.engine = create_engine(f"postgresql+psycopg2://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}")
+        '''
+        This function is used to instantiate a SQLAlchemy engine to connect to a database.
+        
+        Returns:
+            A SQLAlchemy engine connected to the database: self.dbconn
+        '''
+        self.engine = create_engine(f"{self.db_type}+{self.dbapi}://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}")
         print('creating engine')
         self.dbconn = self.engine.connect()
         print('establishing connection')
@@ -41,19 +58,40 @@ class  RDSDatabaseConnector(object):
 
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+        '''This function closes the connection to the database'''
         print('closing connection')
         self.dbconn.close()
-        print('data extracted')
 
-
-def loan_payments_to_dataframe(dbcredentials):
-    with RDSDatabaseConnector(dbcredentials) as conn:
+def loan_payments_to_dataframe(db_credentials_file):
+    '''
+    This function extracts loan payments information to a dataframe
+    
+    Args:
+        dbcredentials (str): filename (without extension) containing the database credentials to allow connection
+    
+    Returns:
+        dataframe 
+        '''
+    with RDSDatabaseConnector(db_credentials_file) as conn:
         df = pd.read_sql_table('loan_payments', conn)
+        # printed line confirms variable name of dataframe
         print('loan_payments data extracted and available in \'loan_payments\' dataframe') 
         return df
 
 def save_to_csv(dataframe):
+    '''
+    This function saves a dataframe as a .csv file
+    
+    Args:
+        dataframe: variable name of dataframe object to be saved as a .csv file
+
+    Returns:
+        .csv file of dataframe
+    '''
+    # dataframe.name ensures that name of dataframe is passed to the filename rather than the dataframe itself
     dataframe.to_csv(f'{dataframe.name}.csv', sep=',', index=False)
+    
+    # printed line confirms name of .csv file that has been created.
     print(f'{dataframe.name} dataframe saved to {dataframe.name}.csv in folder')
 
 if __name__ == '__main__':
